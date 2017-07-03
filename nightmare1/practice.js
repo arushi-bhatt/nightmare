ed9bc56fdc2ae3b3ef857d4656bf7ae429ebde5f
ArushiBhatt <arushi131.bhatt@gmail.com>
2017-07-03 17:06:11 +0530

    Installed JsDom for DOM parsing.

--- nightmare1/practice.js
+++ nightmare1/practice.js
@@ -1,14 +1,20 @@
 var Nightmare = require('nightmare');		
 var nightmare = Nightmare({ show: true });
 
+/*
+const jsdom = require("jsdom");
+const dom = new jsdom.JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+*/
+
 nightmare
   .goto('http://www.seleniumeasy.com/test/basic-first-form-demo.html')
   .type('#user-message', 'Hello My Name is Arushi Bhatt ,:)')
-  //.click('#search_button_homepage')
+  .click('#search_button_homepage')
   //.wait('#zero_click_wrapper .c-info__title a')
-  .evaluate(function () {
-    return document.querySelector('#zero_click_wrapper .c-info__title a').href;
-  })
+  //.evaluate(function () {
+   // return document.querySelector('#zero_click_wrapper .c-info__title a').href;
+  //})
   .end()
   .then(function (result) {
     console.log(result);
